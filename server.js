6b41a445d0da8a5ddd56765300b31726d3e2a016
Rohan <rohansethi1999430@gmail.com>
2021-07-17 10:18:38 +0530

    test of post and get request working now

--- server.js
+++ server.js
@@ -1,5 +1,8 @@
 import express from 'express'
 import mongoose from 'mongoose'
+import Cards from './dbCards.js'
+import Cors from 'cors';
+
 
 
 //App config
@@ -9,7 +12,8 @@ const port=process.env.PORT || 8001
 const connection_url='mongodb+srv://admin:rs.9897637879@cluster0.9sdtr.mongodb.net/tinderdb?retryWrites=true&w=majority'
 
 //Middlewares
-
+app.use(express.json());
+app.use(Cors());
 //DB config
 
 mongoose.connect(connection_url,{
@@ -23,6 +27,35 @@ mongoose.connect(connection_url,{
 app.get('/',(req,res)=>{
     res.status(200).send('Hello from Rohans server!!')
 })
+
+
+app.post("/tinder/card",(req,res)=>{
+    const dbCard=req.body;
+
+    Cards.create(dbCard,(err,data)=>{
+        if(err){
+            res.status(500).send(err);
+        }
+        else{
+            res.status(201).send(data);
+        }
+    });
+});
+
+app.get("/tinder/cards",(req,res)=>{
+    
+
+    Cards.find((err,data)=>{
+        if(err){
+            res.status(500).send(err)
+        }
+        else{
+            res.status(200).send(data)
+        }
+    })
+})
+
+
 
 //Listener
 
