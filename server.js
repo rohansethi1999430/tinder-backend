0cd2066bca82c6ad7d78bad53c19c522d766fcdb
Rohan <rohansethi1999430@gmail.com>
2021-07-17 08:54:55 +0530

    connected to mongo database and created moongoose Schema  in dbCards.js

--- server.js
+++ server.js
@@ -6,10 +6,17 @@ import mongoose from 'mongoose'
 
 const app=express();
 const port=process.env.PORT || 8001
+const connection_url='mongodb+srv://admin:rs.9897637879@cluster0.9sdtr.mongodb.net/tinderdb?retryWrites=true&w=majority'
 
 //Middlewares
 
 //DB config
+
+mongoose.connect(connection_url,{
+    useNewUrlParser:true,
+    useCreateIndex:true,
+    useUnifiedTopology:true,
+})
 
 //API Endpoints
 
